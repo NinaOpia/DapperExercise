b588ebfc20e19edbe399f17768d0e90c20fca3d9
Nina <boldurfaith@gmail.com>
2023-04-30 17:22:04 -0700

    initial commit

---
+++ DapperExercise/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DapperExercise
+{
+	public interface IProductRepository
+	{
+		public IEnumerable<Product> GetAllProducts();
+
+		public void CreateProduct(string name, double price, int categoryID);
+
+		public void UpdateProduct(int productID, string updateedName);
+
+		public void DeleteProduct(int productID);
+	}
+}
+
